284ffca214896c9bdafe5bfd532a2bdbc18a4512
Estudiante <Estudiante@10.203.0.86>
2017-02-06 14:38:15 -0500

    Le cambie cositas

--- src/All.java
+++ src/All.java
@@ -2,6 +2,6 @@ public class All
 {
 	public static void main(String[] arg)
 		{
-			System.out.println("Hola Mundo, te dominare mundo");
+			System.out.println("Hola Mundo, no me domines, mundo");
 		}
 }
